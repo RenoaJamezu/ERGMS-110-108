8f8383d011bf586617188df26dc4d6e35be51e0c
Kurasao <jamerolenor@gmail.com>
2023-12-11 23:51:11 +0800

    remove infos from app.jsx

--- resources/src/App.jsx
+++ resources/src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
-import { Farminfo } from './components/Modal/FarmInfo'
-import { Mobileinfo } from './components/Modal/Mobileinfo'
-import { Promotioninfo } from './components/Modal/Promotioninfo'
-import { Stallinfo } from './components/Modal/Stallinfo'
 import { Mainpage } from "./pages/Mainpage"
 
 function App() {
 
   return (
     <>
-      <Mainpage />
-      <Farminfo />
-      <Stallinfo />
-      <Mobileinfo />
-      <Promotioninfo />
+      <Mainpage />~
     </>
   )
 }
